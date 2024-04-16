a5949c08273a7d85a78c4eda920ac9e0113329e1
davidm8624 <davidm8624@gmail.com>
2024-04-16 15:28:01 -0700

    basic without input

--- br3/dungeon/adventure.rb
+++ br3/dungeon/adventure.rb
@@ -5,23 +5,73 @@ class Dungeon
     @player = player
     @rooms = {}
   end
+
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
+
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
+
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms[reference]
+  end
+  
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player.location).connections[direction]
+  end
+  
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_direction(direction)
+    show_current_description
+  end
 end
 
 class Player
   attr_accessor :name, :location
-
-  def initialize(player_name)
-    @name = player_name
+  def initialize(name)
+    @name = name
   end
 end
 
 class Room
   attr_accessor :reference, :name, :description, :connections
-
-  def initialize(refrence, name, description, connections)
+  def initialize(refernce, name, description, connections)
     @reference = reference
     @name = name
     @description = description
     @connections = connections
   end
+
+  def full_description
+    @name + "\n\nYou are in " + @description
+  end
 end
+
+
+#changed to a struct to simplify but needed more class functionality so switched back.
+
+#Player = Struct.new(:name, :location)
+#Room = Struct.new(:reference, :name, :description, :connections)
+
+##############################################################################
+
+me = Player.new("first last")
+my_dungeon = Dungeon.new(me)
+puts my_dungeon.player.name
+
+#adding rooms
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", { west: :smallcave })
+my_dungeon.add_room(:smallcave, "Small cave", "a small claustrophobic cave", { east: :largecave })
+
+#start the adventure
+my_dungeon.start(:largecave)
+my_dungeon.go(:west)
+my_dungeon.go(:east)
