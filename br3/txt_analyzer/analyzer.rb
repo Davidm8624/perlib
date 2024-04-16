1d5d098d8494d0a91384889cbec37192a11fc60d
davidm8624 <davidm8624@gmail.com>
2024-04-16 14:46:29 -0700

    analyzer done

--- br3/txt_analyzer/analyzer.rb
+++ br3/txt_analyzer/analyzer.rb
@@ -6,3 +6,21 @@ File.open("text.txt").each do |line|
 end
 
 puts "#{line_count} lines"
+
+total_characters = text.length
+puts "#{total_characters} char"
+
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospaces} char without spaces"
+
+word_count = text.split.length
+puts "#{word_count} words"
+
+paragraph_count = text.split(/\n\n/).length
+puts "#{paragraph_count} paragraphs"
+
+sentence_count = text.split(/\.|\?|!/).length
+puts "#{sentence_count} sentences"
+
+puts "#{sentence_count / paragraph_count} sentences per paragraph avg"
+puts "#{word_count / sentence_count} words per sentence avg"
