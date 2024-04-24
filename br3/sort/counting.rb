e0825239c421e21c21bee5e2462670904880bb35
davidm8624 <davidm8624@gmail.com>
2024-04-23 17:23:57 -0700

    entry migration

---
+++ br3/sort/counting.rb
@@ -0,0 +1,18 @@
+def counting_sort(array, max_value)
+  num_counts = Array.new(max_value + 1, 0)
+  array.each do |item|
+    num_counts[item] += 1
+  end
+
+  sorted_array = []
+  num_counts.each_with_index do |count, item|
+    count.times { sorted_array << item }
+  end
+
+  sorted_array
+end
+
+arr = [4, 2, 2, 8, 3, 3, 1]
+max_value = arr.max
+sorted_arr = counting_sort(arr, max_value)
+puts "Counted array = #{sorted_arr}"
---
+++ br3/sort/insertion.rb
@@ -0,0 +1,16 @@
+def insertion_sort(array)
+  (1...array.length).each do |i|
+    key = array[i]
+    j = i - 1
+    while j >= 0 && array[j] > key
+      array[j + 1] = array[j]
+      j -= 1
+    end
+    array[j + 1] = key
+  end
+  array
+end
+
+arr = [22, 11, 99, 88, 9, 7, 42]
+sorted_arr = insertion_sort(arr)
+puts "Inserted array = #{sorted_arr}"
