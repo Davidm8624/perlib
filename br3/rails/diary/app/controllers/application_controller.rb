b13627808666cf549b103d2378985e770e33752b
davidm8624 <davidm8624@gmail.com>
2024-04-24 21:00:28 -0700

    visit counter

--- br3/rails/diary/app/controllers/application_controller.rb
+++ br3/rails/diary/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+#counts the ammount of times the user had been to the site using cookies
+  before_action :count_request_in_session
+  
+  def count_request_in_session
+    session[:request] ||=0 #starts it at 0 if it didnt detect a request value yet
+    session[:request] +=1 
+  end
 end
