11223e760bdad89b8595a8c1074cc0888d0d5782
davidm8624 <davidm8624@gmail.com>
2024-05-22 17:22:07 -0700

    a

---
+++ workshop/oop/socialShare/main.rb
@@ -0,0 +1,23 @@
+class SocialShare
+  @@platforms = {
+    facebook: "https://www.facebook.com/sharer/sharer.php?u=%{url}",
+    x: "https://x.com/intent/tweet?text=%{url} %{title}",
+    linkedin: "http://www.linkedin.com/shareArticles?mini=true&url=%{url}&title=%{title}&summary=%{summary}&source=Recognize"
+  }
+    
+  def initialize(platform, title, url, summary = "")
+    @platform = platform.to_sym
+    @share_url = url
+    @title = title
+    @summary = summary
+  end
+  
+  def generate_url
+    @@platforms[@platform % {title: @title, url: @share_url, summary: @summary}]
+  end
+  
+  def url
+    @url ||= generate_url
+  end
+  
+end
